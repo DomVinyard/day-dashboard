ef48e4d120e9676491185ae7dad32e759fccc0fb
Dom Vinyard <domvinyard@me.com>
2022-06-22 09:48:49 +0100

    timeline

--- src/app.js
+++ src/app.js
@@ -9,22 +9,67 @@ import "./app.css";
 const longitude = 33.5731;
 const latitude = 7.5898;
 
-const LATE_STARTS_AT = 0;
-const SUNRISESET_MINS = 60;
+const LATE_STARTS_AT = 23;
+const SUNRISESET_MINS = 120;
 const VERY_LATE_STARTS_AT = 2;
 const LUNCH_STARTS_AT = 12;
 const LUNCH_ENDS_AT = 14;
 
+Number.prototype.round = function (places) {
+  return +(Math.round(this + "e+" + places) + "e-" + places);
+};
+
 const formatDate = (date) => DateTime.fromJSDate(date);
+
 export default function App() {
   const [message, setMessage] = useState();
   const [now, setNow] = useState(DateTime.now());
+  // const [day, setDay] = useState();
+  const [clock, setClock] = useState();
   useInterval(() => setNow(DateTime.now()), 500);
   useEffect(() => {
-    const generateMessage = (latitude, longitude) => {
+    const sunrise = formatDate(getSunrise(latitude, longitude));
+    const sunset = formatDate(getSunset(latitude, longitude));
+    const isBeforeSunrise = now < sunrise;
+    const buildClock = () => {
+      const timestampsAbsolute = Object.entries({
+        now,
+        sunrise: sunrise,
+        morning: sunrise.plus({ minutes: SUNRISESET_MINS / 2 }),
+        lunch: now.set({ hour: LUNCH_STARTS_AT, minute: 0 }),
+        afternoon: now.set({ hour: LUNCH_ENDS_AT, minute: 0 }),
+        evening: sunset.plus(2, "hours"),
+        late: now.set({ hour: LATE_STARTS_AT, minute: 0 }),
+        veryLate: now
+          .set({ hour: VERY_LATE_STARTS_AT, minute: 0 })
+          .plus({ day: 1 }),
+        nextSunrise: sunrise.plus({ day: 1 }),
+      }).reduce((acc, [key, value]) => {
+        return {
+          ...acc,
+          [key]: value
+            .minus({ day: now < sunrise && key != "now" ? 1 : 0 })
+            .toMillis(),
+        };
+      }, {});
+
+      const timestampsRatio = Object.entries(timestampsAbsolute).reduce(
+        (acc, [key, value]) => {
+          return {
+            ...acc,
+            [key]: (
+              (value - timestampsAbsolute.sunrise) /
+              (timestampsAbsolute.nextSunrise - timestampsAbsolute.sunrise)
+            ).round(3),
+          };
+        },
+        {}
+      );
+
+      return { ...timestampsRatio };
+    };
+    const generateMessage = () => {
       const prevDayName = now.minus(1, "day").weekdayLong;
-      const sunrise = formatDate(getSunrise(latitude, longitude));
-      const isBeforeSunrise = now < sunrise;
       if (isBeforeSunrise) {
         const sunriseMins = Math.abs(now.diff(sunrise, "minutes").minutes);
         if (sunriseMins < SUNRISESET_MINS / 2)
@@ -39,19 +84,41 @@ export default function App() {
       if (isLunchTime) return `${now.weekdayLong} lunchtime 🍽`;
       const isBeforeLunch = now.hour < LUNCH_STARTS_AT;
       if (isBeforeLunch) return `${now.weekdayLong} morning`;
-      const sunset = formatDate(getSunset(latitude, longitude));
       const beforeEvening = now < sunset.minus(1, "hour");
       if (beforeEvening) return `${now.weekdayLong} afternoon`;
       const isEvening = now < sunset.plus(2, "hours");
       if (isEvening) return `${now.weekdayLong} evening`;
       return `${now.weekdayLong} night`;
     };
-    setMessage(`It's ${generateMessage(longitude, latitude)}`);
+    setClock(buildClock());
+    setMessage(`It's ${generateMessage()}`);
   }, [now]);
+  // console.log(clock);
+  if (!clock) return "loading";
+  const phases = [
+    { key: "sunrise", color: "#E8B562" },
+    { key: "morning", color: "#F8E16A" },
+    { key: "lunch", color: "#FAFBC3" },
+    { key: "afternoon", color: "#B0CADD" },
+    {
+      key: "evening",
+      color: "#6793B4",
+    },
+    {
+      key: "late",
+      color: "#254885",
+    },
+    {
+      key: "veryLate",
+      color: "#112A74",
+    },
+    { key: "nextSunrise" },
+  ];
+  const height = 28;
   return (
     <Center width="full" height="100vh" flexDirection="column">
       <Flex
-        opacity="0.3"
+        // opacity="0.3"
         fontSize="60px"
         fontWeight="black"
         flexDirection="row"
@@ -63,6 +130,39 @@ export default function App() {
       </Flex>
       <Box>
         <Text>{message || "loading"}</Text>
+      </Box>
+      <Box w={"90%"} maxW="400px" mt={4}>
+        <Flex h={`${height}px`}>
+          {phases.map((phase, i) => {
+            if (!phases[i + 1]) return null;
+            const starts = clock[phase.key];
+            const ends = clock[phases[i + 1].key];
+            const percent = (ends - starts) * 100;
+            return (
+              <Box
+                // bgPos={"center center"}
+                // bgSize="cover"
+                // bgImg={`url(${phase.image})`}
+                mr={"1px"}
+                bg={phase.color}
+                width={`${percent}%`}
+              >
+                {/* {phase} starts: {starts} ends: {ends} */}
+              </Box>
+            );
+          })}
+        </Flex>
+        <Flex
+          mt={`-${height}px`}
+          // bg={"white"}
+          position="relative"
+          zIndex={2}
+          borderRight={"3px solid red"}
+          // opacity={now.millisecond < 500 ? 1 : 0.8}
+          w={`${clock.now * 100}%`}
+          // w={"100%"}
+          h={`${height}px`}
+        ></Flex>
       </Box>
     </Center>
   );
